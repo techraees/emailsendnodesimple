4722bf577a86c4f6e55edc9ba20b6da1c872b38f
oioidev <rana.raees@oioi.biz>
2024-07-02 14:04:43 +0500

    Add send email for sendotp route." git push -u origin main

--- index.js
+++ index.js
@@ -57,6 +57,48 @@ app.post("/send", async (req, res) => {
     console.error("Error occurred while sending email", error.message);
   }
 });
+
+
+// Endpoint to send emails
+app.post("/sendotp", async (req, res) => {
+  let { email, subject, html, text } = req.body;
+  if (!email || !subject || !html ) {
+    return res
+      .status(400)
+      .json({ status: "fail", message: "Validate Req Body" });
+  }
+
+  try {
+    // Sending Email To Email
+    const emailSuccess = await sendEmail({
+      options: {
+        email,
+        subject,
+        text,
+        html,
+      },
+    });
+
+    if (!emailSuccess) {
+      return res
+        .status(400)
+        .json({ status: "fail", message: "Error While Sending Email" });
+    }
+    return res.status(200).json({
+      status: "success",
+      message: "Deleted Successfully",
+      mail: "Mail Send Successfully",
+    });
+  } catch (error) {
+    return res.status(200).json({
+      status: "fail",
+      message: "Error While Email Send",
+      mail: "Error While Email Send",
+    });
+    console.log(error)
+    console.error("Error occurred while sending email", error.message);
+  }
+});
 // Endpoint to send emails
 app.get("/", async (req, res) => {
   return res
